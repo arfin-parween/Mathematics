328e3883f69de6693f24c54cb511d5ee3336a38c
Arfin Parween <wbstartpracticing@gmail.com>
2023-12-27 17:45:08 +0530

    Added SimpleCalculator

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter 1st Integer");
+        int a= sc.nextInt();
+        System.out.println("Enter 2nd Integer");
+        int b =sc.nextInt();
+        System.out.println("Addition is " + (a+b));
+        System.out.println("Subtraction is " +(a-b));
+        System.out.println("Multiplication is " + (a*b));
+        System.out.println("Division is "+ (a/b));
+    }
+}
