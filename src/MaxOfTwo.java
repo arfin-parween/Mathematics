029af890ee10a87c5365baa16d12d39a1b39f444
Arfin Parween <wbstartpracticing@gmail.com>
2023-12-28 01:50:01 +0530

    Added Maxof numbers

---
+++ src/MaxOfTwo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MaxOfTwo {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 1st Number");
+        int a= sc.nextInt();
+        System.out.println("Enter 2nd Number");
+        int b =sc.nextInt();
+        int output = Solution(a,b);
+        System.out.println("Maximum is "+ output);
+    }
+    static int Solution(int num1, int num2){
+        if(num1>num2) return num1;
+        else return  num2;
+    }
+}
--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+// Program for simple calculator
 
 public class SimpleCalculator {
     public static void main(String[] args) {
